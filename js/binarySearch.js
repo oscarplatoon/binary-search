00f479a094e915196a446121b2bca5cd679480ce
Jonathan Young <jonyoungg@gmail.com>
2017-02-07 15:32:00 -0600

    remove broken spec file

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,2 +1,13 @@
-exports.binarySearch = function() {
-};
+function binarySearch(){
+  // Code goes here!
+}
+
+var smallArray = [1,2,3,4,5]
+var largeArray = [1,5,7,2,3,8,4,9]
+
+console.log(binarySearch(1, smallArray) === 0);
+console.log(binarySearch(2, smallArray) === 1);
+console.log(binarySearch(3, smallArray) === 2);
+console.log(binarySearch(4, smallArray) === 3);
+console.log(binarySearch(5, smallArray) === 4);
+console.log(binarySearch(7, largeArray) === 5);
--- js/binarySearchSpec.js
+++
@@ -1,9 +0,0 @@
-var bs = require("./binarySearch");
-
-console.log(bs.binarySearch(1, [1,2,3,4,5]) === 0);
-console.log(bs.binarySearch(2, [1,2,3,4,5]) === 1);
-console.log(bs.binarySearch(3, [1,2,3,4,5]) === 2);
-console.log(bs.binarySearch(4, [1,2,3,4,5]) === 3);
-console.log(bs.binarySearch(5, [1,2,3,4,5]) === 4);
-
-console.log(bs.binarySearch(7, [1,5,7,2,3,8,4,9]) === 5);
