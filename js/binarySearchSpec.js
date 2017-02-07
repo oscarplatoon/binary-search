b2d8df6e0175290244fe9e16ca138306cc28f5b8
Jonathan Young <jonyoungg@gmail.com>
2017-02-07 10:46:11 -0600

    Update binarySearchSpec.js

--- js/binarySearchSpec.js
+++ js/binarySearchSpec.js
@@ -1,8 +1,11 @@
 var bs = require("./binarySearch"),
-  test_array = ['A','B','C','D','E'];
+var test_array = [1,2,3,4,5];
+var super_big_array = [1,5,7,2,3,8,4,9];
 
-console.log(bs.binarySearch("A", test_array) === 0);
-console.log(bs.binarySearch("B", test_array) === 1);
-console.log(bs.binarySearch("C", test_array) === 2);
-console.log(bs.binarySearch("D", test_array) === 3);
-console.log(bs.binarySearch("E", test_array) === 4);
+console.log(bs.binarySearch(1, test_array) === 0);
+console.log(bs.binarySearch(2, test_array) === 1);
+console.log(bs.binarySearch(3, test_array) === 2);
+console.log(bs.binarySearch(4, test_array) === 3);
+console.log(bs.binarySearch(5, test_array) === 4);
+
+console.log(bs.binarySearch(7, super_big_array) === 5);
