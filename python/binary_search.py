6e302acd40d9c5f5c3d916c807ad2cbbe2ae5962
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-07 15:58:15 -0400

    Recursive logic using pointers complete and functional. Test cases built

--- python/binary_search.py
+++ python/binary_search.py
@@ -3,39 +3,24 @@ import random
 values = random.sample(list(range(1, 10000)), 1000)
 values.sort()
 
+
 def binary_search(value_to_find, array_to_search, start, end):
     result = -1
-
-    middle_index = len(array_to_search)//2
-    print(f"The middle index is: {middle_index}")
-    if value_to_find == (array_to_search[middle_index]):
-        return middle_index
-    if len(array_to_search) == 1:
+    #Have to catch unsorted arrays, as they are included in the test cases.
+    array_to_search.sort()
+    #Base cases: The start/end pointers will catch the case of no more array to search, and return False/-1/None case.
+    if end - start + 1 <= 0:
         return result
+    #If the middle index is the value, it'll return the index.
+    middle_index = start + (end-start)//2
+    if array_to_search[middle_index] == value_to_find:
+        return middle_index
 
+    # This logic checks the target against the array and if lower, 
+    # recurses to the lower half of the searched array, and if higher, 
+    # to the upper half using manipulation of the middle index, start, 
+    # and end pointers as they are passed back into the function.
     if value_to_find < (array_to_search[middle_index]):
-        binary_list = array_to_search[:middle_index]
-        print(binary_list)
-        binary_search(value_to_find, binary_list)
+        return(binary_search(value_to_find, array_to_search, start, middle_index-1))
     elif value_to_find > (array_to_search[middle_index]):
-        binary_list = array_to_search[middle_index:]
-        print(binary_list)
-        binary_search(value_to_find, binary_list)
-
-
-    return result
-
-# Assumptions, list is sorted when passed in, use recursion
-# array_to_search[middle_index] < val to find
-# array_to_search[middle_index] > val to find
-# # array_to_search[middle_index] == val to find
-# Base case is when the middle_index is the value to find. Return middle index
-# Exit condition, if the value to find is not in the list is when the length of the list is 1
-# How to track the halving of the arrays between recursions. counter? 
-
-
-super_big_array = [1, 5, 7, 2, 3, 8, 4, 9]
-super_big_array.sort()
-# print(binary_search(3, [1, 2, 3, 4]))
-# print(binary_search(6, super_big_array))
-print(binary_search(7, super_big_array))
+        return(binary_search(value_to_find, array_to_search, middle_index+1, end))
--- python/binary_search_spec.py
+++ python/binary_search_spec.py
@@ -7,7 +7,7 @@ class TestBinarySearch(unittest.TestCase):
     Test binary search returns an integer:
     """
     def test_returns_a_value(self):
-        self.assertEqual(type(binary_search(3, [1,2,3,4])), type(4))
+        self.assertEqual(type(binary_search(3, [1,2,3,4], 0, 4)), type(4))
 
     """
     Test Binary Search:
@@ -15,15 +15,15 @@ class TestBinarySearch(unittest.TestCase):
     def test_accuracy_of_search(self):
         test_array = [1,2,3,4,5]
 
-        self.assertTrue(binary_search(1, test_array) == 0)
+        self.assertTrue(binary_search(1, test_array, 0, len(test_array)) == 0)
 
-        self.assertTrue(binary_search(2, test_array) == 1)
+        self.assertTrue(binary_search(2, test_array, 0, len(test_array) == 1))
 
-        self.assertTrue(binary_search(3, test_array) == 2)
+        self.assertTrue(binary_search(3, test_array, 0, len(test_array) == 2))
 
-        self.assertTrue(binary_search(4, test_array) == 3)
+        self.assertTrue(binary_search(4, test_array, 0, len(test_array)) == 3)
 
-        self.assertTrue(binary_search(5, test_array) == 4)
+        self.assertTrue(binary_search(5, test_array, 0, len(test_array)) == 4)
 
     """
     Test Larger Arrays to confirm functionality:
@@ -32,9 +32,9 @@ class TestBinarySearch(unittest.TestCase):
     def test_large_array_accuracy(self):
         super_big_array = [1,5,7,2,3,8,4,9]
 
-        self.assertTrue(binary_search(7, super_big_array) == 5)
+        self.assertTrue(binary_search(7, super_big_array, 0, len(super_big_array)) == 5)
 
-        self.assertTrue(binary_search(6, super_big_array) == -1)
+        self.assertTrue(binary_search(6, super_big_array, 0, len(super_big_array)) == -1)
 
 if __name__ == '__main__':
     unittest.main()
