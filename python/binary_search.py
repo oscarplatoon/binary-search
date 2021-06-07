e04e3084197c6b2dfef17f092576bd20a13d082c
stesun2 <ss27@illinois.edu>
2021-06-07 14:56:38 -0500

    started binary_search.py

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,19 @@
-def binary_search():
-    pass
+
+# 1. Say you're searching for a value 'number_to_find' in a search set of sorted_values
+# 2. Find the middle value in 'sorted_values' we'll call 'middle_value'
+# 3. If 'number_to_find' is equal to 'middle_value' then your target is found
+# 4. If 'number_to_find' is less than 'middle_value', split your 'sorted_values' in half and repeat the algorithm on your new subset (the half of 'sorted_values' before 'middle_value')
+# 5. If 'number_to_find' is greater than 'middle_value', repeat step but with the half of 'sorted_values' values after 'middle_value'
+# 6. Repeat until you find 'number_to_find' or return -1 if it doesn't exist
+
+def binary_search(num, arr):
+
+    return arr
+
+
+def sort_array(arr):
+
+    sorted_array = arr
+
+    return sorted_array
+
--- python/binary_search_spec.py
+++ python/binary_search_spec.py
@@ -1,16 +1,36 @@
 # Write your unit tests here
-
+import unittest
 from binary_search import binary_search
 
 test_array = [1,2,3,4,5]
-
-print(binary_search(1, test_array) == 0)
-print(binary_search(2, test_array) == 1)
-print(binary_search(3, test_array) == 2)
-print(binary_search(4, test_array) == 3)
-print(binary_search(5, test_array) == 4)
-
-
 super_big_array = [1,5,7,2,3,8,4,9]
-print(binary_search(7, super_big_array) == 5)
-print(binary_search(6, super_big_array) == -1)
+
+class BinarySearchTest(unittest.TestCase):
+    """Tests for binary_search.py"""
+
+    def test_array_input(self):
+        self.assertEqual(binary_search(test_array), test_array)
+
+    def test_simple_array(self):
+        self.assertEqual(binary_search(test_array), test_array)
+        self.assertEqual(binary_search(1, test_array) == 0)
+        self.assertEqual(binary_search(2, test_array) == 1)
+        self.assertEqual(binary_search(3, test_array) == 2)
+        self.assertEqual(binary_search(4, test_array) == 3)
+        self.assertEqual(binary_search(5, test_array) == 4)
+
+    def test_big_array(self):
+        self.assertEqual(binary_search(7, super_big_array) == 5)
+        self.assertEqual(binary_search(6, super_big_array) == -1)
+
+# print(binary_search(1, test_array) == 0)
+# print(binary_search(2, test_array) == 1)
+# print(binary_search(3, test_array) == 2)
+# print(binary_search(4, test_array) == 3)
+# print(binary_search(5, test_array) == 4)
+
+# print(binary_search(7, super_big_array) == 5)
+# print(binary_search(6, super_big_array) == -1)
+
+if __name__ == '__main__':
+    unittest.main()
