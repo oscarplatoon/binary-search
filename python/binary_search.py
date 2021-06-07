b4368c3d36276934e11eb0a627849f8ebccd8298
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-07 13:42:23 -0500

    Shawn's partial binary search solution

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,22 @@
-def binary_search():
-    pass
+
+def middle_index(sorted_values):
+    i = len(sorted_values)//2
+    return (i if len(sorted_values)%2 == 0 else i + 1)
+
+def binary_search(number_to_find, sorted_values):
+    middle_i = middle_index(sorted_values)
+    middle_value = sorted_values[middle_i]
+    print(middle_value)
+    if number_to_find == sorted_values[middle_i]:
+        print("Number found")
+        return middle_i
+    elif len(sorted_values) == 1:
+        print("Number not found")
+        return -1
+    elif number_to_find < middle_value:
+        print("Number smaller than middle value")
+        return binary_search(number_to_find, sorted_values[0:middle_i])
+    elif number_to_find > middle_value:
+        print("Number larger than middle value")
+        return binary_search(number_to_find, sorted_values[middle_i+1:])
+
--- python/binary_search_spec.py
+++ python/binary_search_spec.py
@@ -1,16 +1,34 @@
 # Write your unit tests here
-
+import unittest
 from binary_search import binary_search
+from binary_search import middle_index
 
-test_array = [1,2,3,4,5]
+class BinarySearchUnitTest(unittest.TestCase):
 
-print(binary_search(1, test_array) == 0)
-print(binary_search(2, test_array) == 1)
-print(binary_search(3, test_array) == 2)
-print(binary_search(4, test_array) == 3)
-print(binary_search(5, test_array) == 4)
+    def test_middle_index_odd(self):
+        self.assertEqual(middle_index([1,2,3,4,5]), 3)
+    
+    def test_middle_index_even(self):
+        self.assertEqual(middle_index([1,2,3,4]), 2)
+
+    def test_odd_search(self):
+        self.assertEqual(binary_search(2, [1,2,3,4,5]), 1)
+
+    def test_even_search(self):
+        self.assertEqual(binary_search(6, [1,2,3,4,5,6]), 5)
+        
+if __name__ == '__main__':
+    unittest.main()
+
+# test_array = [1,2,3,4,5]
+
+# print(binary_search(1, test_array) == 0)
+# print(binary_search(2, test_array) == 1)
+# print(binary_search(3, test_array) == 2)
+# print(binary_search(4, test_array) == 3)
+# print(binary_search(5, test_array) == 4)
 
 
-super_big_array = [1,5,7,2,3,8,4,9]
-print(binary_search(7, super_big_array) == 5)
-print(binary_search(6, super_big_array) == -1)
+# super_big_array = [1,5,7,2,3,8,4,9]
+# print(binary_search(7, super_big_array) == 5)
+# print(binary_search(6, super_big_array) == -1)
