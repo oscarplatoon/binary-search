d99665751ef1dc2df6179081ee5f6ac6d8ae7262
jakewhite5 <jakewhite5@yahoo.com>
2021-06-07 14:37:59 -0700

    python solution with unittesting

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,20 @@
-def binary_search():
-    pass
+def binary_search(num, array):
+    array.sort()
+    return_index = -1
+    current_array = array
+    index_start = 0
+    while len(current_array) > 1:
+        middle_index = round((len(current_array))/2)
+        middle_num = current_array[round((len(current_array))/2)]
+        if num == middle_num:
+            return_index = middle_index + index_start
+            return return_index
+        elif num > middle_num:
+            index_start += middle_index
+            current_array = current_array[middle_index:]
+        elif num < middle_num:
+            current_array = current_array[:middle_index]
+    if current_array[0] == num:
+        return 0 + index_start
+    else:
+        return return_index
--- python/binary_search_spec.py
+++ python/binary_search_spec.py
@@ -1,8 +1,26 @@
 # Write your unit tests here
-
+import unittest
 from binary_search import binary_search
 
 test_array = [1,2,3,4,5]
+super_big_array = [1,5,7,2,3,8,4,9]
+
+class CaesarCipher(unittest.TestCase):
+
+    def test_for_functionality1(self):
+        self.assertEqual(binary_search(1, test_array), 0)
+
+    def test_for_functionality2(self):
+        self.assertEqual(binary_search(3, test_array), 2)
+
+    def test_for_functionality3(self):
+        self.assertEqual(binary_search(7, super_big_array), 5)
+
+    def test_for_functionality4(self):
+        self.assertEqual(binary_search(6, super_big_array), -1)
+    
+if __name__ == '__main__':
+    unittest.main()
 
 print(binary_search(1, test_array) == 0)
 print(binary_search(2, test_array) == 1)
