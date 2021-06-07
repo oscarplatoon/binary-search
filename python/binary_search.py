dfb5829425c2de1d86138243a6ec40f41d248e65
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-07 15:19:19 -0400

    Partially working, need to better track index values of base input array. Experimenting with start/end points.

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,41 @@
-def binary_search():
-    pass
+import random
+
+values = random.sample(list(range(1, 10000)), 1000)
+values.sort()
+
+def binary_search(value_to_find, array_to_search, start, end):
+    result = -1
+
+    middle_index = len(array_to_search)//2
+    print(f"The middle index is: {middle_index}")
+    if value_to_find == (array_to_search[middle_index]):
+        return middle_index
+    if len(array_to_search) == 1:
+        return result
+
+    if value_to_find < (array_to_search[middle_index]):
+        binary_list = array_to_search[:middle_index]
+        print(binary_list)
+        binary_search(value_to_find, binary_list)
+    elif value_to_find > (array_to_search[middle_index]):
+        binary_list = array_to_search[middle_index:]
+        print(binary_list)
+        binary_search(value_to_find, binary_list)
+
+
+    return result
+
+# Assumptions, list is sorted when passed in, use recursion
+# array_to_search[middle_index] < val to find
+# array_to_search[middle_index] > val to find
+# # array_to_search[middle_index] == val to find
+# Base case is when the middle_index is the value to find. Return middle index
+# Exit condition, if the value to find is not in the list is when the length of the list is 1
+# How to track the halving of the arrays between recursions. counter? 
+
+
+super_big_array = [1, 5, 7, 2, 3, 8, 4, 9]
+super_big_array.sort()
+# print(binary_search(3, [1, 2, 3, 4]))
+# print(binary_search(6, super_big_array))
+print(binary_search(7, super_big_array))
--- python/binary_search_spec.py
+++ python/binary_search_spec.py
@@ -1,16 +1,40 @@
-# Write your unit tests here
+import unittest
 
 from binary_search import binary_search
 
-test_array = [1,2,3,4,5]
+class TestBinarySearch(unittest.TestCase):
+    """
+    Test binary search returns an integer:
+    """
+    def test_returns_a_value(self):
+        self.assertEqual(type(binary_search(3, [1,2,3,4])), type(4))
 
-print(binary_search(1, test_array) == 0)
-print(binary_search(2, test_array) == 1)
-print(binary_search(3, test_array) == 2)
-print(binary_search(4, test_array) == 3)
-print(binary_search(5, test_array) == 4)
+    """
+    Test Binary Search:
+    """
+    def test_accuracy_of_search(self):
+        test_array = [1,2,3,4,5]
 
+        self.assertTrue(binary_search(1, test_array) == 0)
 
-super_big_array = [1,5,7,2,3,8,4,9]
-print(binary_search(7, super_big_array) == 5)
-print(binary_search(6, super_big_array) == -1)
+        self.assertTrue(binary_search(2, test_array) == 1)
+
+        self.assertTrue(binary_search(3, test_array) == 2)
+
+        self.assertTrue(binary_search(4, test_array) == 3)
+
+        self.assertTrue(binary_search(5, test_array) == 4)
+
+    """
+    Test Larger Arrays to confirm functionality:
+    """
+
+    def test_large_array_accuracy(self):
+        super_big_array = [1,5,7,2,3,8,4,9]
+
+        self.assertTrue(binary_search(7, super_big_array) == 5)
+
+        self.assertTrue(binary_search(6, super_big_array) == -1)
+
+if __name__ == '__main__':
+    unittest.main()
