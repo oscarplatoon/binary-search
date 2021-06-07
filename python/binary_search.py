dd51a27e0d80ad035e410e9dcab3a0892436af53
donkelly55331 <donkelly55331@gmail.com>
2021-06-07 16:22:00 -0500

    Don & Tim completed binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,18 @@
-def binary_search():
-    pass
+import math
+
+def binary_search(object, array):
+    array.sort()
+    lo = 0
+    hi = len(array) -1
+    index = math.floor(hi/2)
+    while True:
+        if array[index] == object:
+            return index
+        elif lo > hi:
+            return -1
+        elif array[index] > object:
+            hi = index -1
+            index = lo + math.floor((index - lo)/2)
+        else:
+             lo = index +1
+             index = index + math.floor((hi + 1 - index)/ 2)
