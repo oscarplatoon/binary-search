350b638e221f0bd2468a111d8b186fddf2fcab8c
Jeremiah Mauga <miahmauga@gmail.com>
2021-06-07 18:19:54 -0700

    Jeremiah's struggle for binary_search

--- python/binary_search_spec.py
+++ python/binary_search_spec.py
@@ -1,14 +1,35 @@
-# Write your unit tests here
+import unittest
 
 from binary_search import binary_search
 
-test_array = [1,2,3,4,5]
+class TestFunctionName(unittest.TestCase) :
+    
+    def test_binary_0(self) :
+        test_array = [1,2,3,4,5]
+        self.assertEqual(binary_search(1,test_array), 0)
+    def test_binary_1(self) :
+        test_array = [1,2,3,4,5]
+        self.assertEqual(binary_search(2,test_array), 1)
+    def test_binary_2(self) :
+        test_array = [1,2,3,4,5]
+        self.assertEqual(binary_search(3,test_array), 2)
+    def test_binary_3(self) :
+        test_array = [1,2,3,4,5]
+        self.assertEqual(binary_search(4,test_array), 3)
+    def test_binary_4(self) :
+        test_array = [1,2,3,4,5]
+        self.assertEqual(binary_search(5,test_array), 4)
 
-print(binary_search(1, test_array) == 0)
-print(binary_search(2, test_array) == 1)
-print(binary_search(3, test_array) == 2)
-print(binary_search(4, test_array) == 3)
-print(binary_search(5, test_array) == 4)
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+# print(binary_search(1, test_array) == 0)
+# print(binary_search(2, test_array) == 1)
+# print(binary_search(3, test_array) == 2)
+# print(binary_search(4, test_array) == 3)
+# print(binary_search(5, test_array) == 4)
 
 
 super_big_array = [1,5,7,2,3,8,4,9]
