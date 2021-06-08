6f8aac5990f6cfb1a6b4a6441a9904e6f5c6a532
stesun2 <ss27@illinois.edu>
2021-06-07 21:52:50 -0500

    completed iterative binary search, need to make more edge test cases

--- python/binary_search.py
+++ python/binary_search.py
@@ -6,14 +6,35 @@
 # 5. If 'number_to_find' is greater than 'middle_value', repeat step but with the half of 'sorted_values' values after 'middle_value'
 # 6. Repeat until you find 'number_to_find' or return -1 if it doesn't exist
 
-def binary_search(num, arr):
+def binary_search(tgt, arr):
+    # Built in python sort method because I am lazy
+    sort_array = sorted(arr)
+    print('sorted array:', sort_array)
+    low = 0
+    high = len(sort_array) - 1
 
-    return arr
+    # iterative approach set arr length low and high
+    while low <= high:
+        # mid will be our split
+        mid = (low + high) // 2
 
+        # if our target equal to our mid value return the index of that value
+        if tgt == sort_array[mid]:
+            print('new mid:', mid)
+            return mid
 
-def sort_array(arr):
+        # if our tgt is less than the lower half of the array
+        elif tgt < sort_array[mid]:
+            high = mid - 1
+            print('high', high)
 
-    sorted_array = arr
+        # if our tgt is greater than lower half
+        else:
+            low = mid + 1
+            print('low', low)
+    return -1
 
-    return sorted_array
+# super_big_array = [1,5,7,2,3,8,4,9]
+super_array = [12,43,55,65,68,90,35,69,22,25,29,31,50,100,101,110]
 
+print(binary_search(6, super_array))
