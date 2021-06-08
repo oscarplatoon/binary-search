67f9b21e2903bec2da30eb12e3fa0ae8ecc85525
Jeremiah Mauga <miahmauga@gmail.com>
2021-06-07 19:04:59 -0700

    Jeremiah's attempt for binary-search

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,13 +1,23 @@
 const binarySearch = (num, array) => {
-  
+  //  Pass in a num and an array to search through
+  //  Find the middle value inside of the array and assign it to a variable
+  //  Create an if statement to see if num == middle, if true return the index
+  //  If num == middle is false, split the array in half and repeat the comparison
+  //  Repeat this step if num == middle is false until true
+  //  Return the index if found, return a -1 if not found
+  count = 0;
+  for(var i = 0; i < array.length; i++) {
+    count += 1
+  }
+  let middle = Math.floor(count / 2);
+  console.log(middle);
 
-  
-
-
+  let first_half = array.slice(middle);
+  console.log(first_half);
 };
 
 
-module.exports = function
+module.exports = binarySearch
 
-
+binarySearch(1, [1,2,3,4,5])
 
--- js/binarySearch.test.js
+++ js/binarySearch.test.js
@@ -2,27 +2,27 @@ const binary = require("./binarySearch");
 
 test("Binary Search One", () => {
   var smallArray = [1,2,3,4,5]
-  expect(binary(1, smallArray))===(anything);
+  expect(binary(1, smallArray))===(0);
 });
 
 test("Binary Search Two", () => {
   var smallArray = [1,2,3,4,5]
-  expect(binary(2, smallArray))===(anything);
+  expect(binary(2, smallArray))===(2);
 });
 
 test("Binary Search Three", () => {
   var smallArray = [1,2,3,4,5]
-  expect(binary(3, smallArray))===(anything);
+  expect(binary(3, smallArray))===(3);
 });
 
 test("Binary Search Four", () => {
   var smallArray = [1,2,3,4,5]
-  expect(binary(4, smallArray))===(anything);
+  expect(binary(4, smallArray))===(4);
 });
 
 test("Binary Search Five", () => {
   var smallArray = [1,2,3,4,5]
-  expect(binary(5, smallArray))===(anything);
+  expect(binary(5, smallArray))===(5);
 });
 
 
